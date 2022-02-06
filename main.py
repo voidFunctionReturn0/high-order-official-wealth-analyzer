f5cd13e748f7c4d7aeda3a7773ba3edf37741924
minsoo Kim <voidfunctionreturn0@gmail.com>
2022-02-06 14:23:45 +0900

    print

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ async def root():
     # csv -> postgres
     engine = create_engine(SQLALCHEMY_DATABASE_URL)
     df.to_sql('asset', con = engine, if_exists='append')
-    # print(engine.execute("SELECT * FROM asset").fetchall())
+    print(engine.execute("SELECT * FROM asset").fetchone())
 
 
     return {"data": "data"}
